b03730732404cfcbfc0f7092e75bdeb06f61a2c8
Your Name <you@example.com>
2023-02-19 06:09:06 -0800

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print("char: %i byte(s)\n", sizeof(char));
+print("int: %i byte(s)\n", sizeof(int));
+print("long int: %i byte(s)\n", sizeof(long int));
+print("long long int: %i byte(s)\n", sizeof(long long int));
+print("float: %i byte(s)\n", sizeof(float));
+return (0);
+}
