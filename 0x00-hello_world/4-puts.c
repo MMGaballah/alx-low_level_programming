ce2626a82638d04e176f945ae1df609d7ec7efb1
Your Name <you@example.com>
2023-02-19 05:29:40 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -entry
- * return always 0
- * */
+ * main - Entry point
+ * return: always 0
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
