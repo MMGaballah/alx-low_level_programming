ecf40abc942b8f2a65a3bbfaa97d2e335825d1cd
Your Name <you@example.com>
2023-02-19 06:12:12 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-print("char: %i byte(s)\n", sizeof(char));
-print("int: %i byte(s)\n", sizeof(int));
-print("long int: %i byte(s)\n", sizeof(long int));
-print("long long int: %i byte(s)\n", sizeof(long long int));
-print("float: %i byte(s)\n", sizeof(float));
+printf("char: %i byte(s)\n", sizeof(char));
+printf("int: %i byte(s)\n", sizeof(int));
+printf("long int: %i byte(s)\n", sizeof(long int));
+printf("long long int: %i byte(s)\n", sizeof(long long int));
+printf("float: %i byte(s)\n", sizeof(float));
 return (0);
 }
