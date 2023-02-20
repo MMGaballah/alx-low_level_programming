b68c44283d9d21f8cb2cd2a95be544e9efd407bd
Your Name <you@example.com>
2023-02-20 10:48:03 -0800

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 char a;
 char b;
-for(a = 'a'; a <= 'z'; a++)
+for (a = 'a'; a <= 'z'; a++)
 {
 putchar(a);
 }
-for(b = 'A'; a <= 'Z'; b++)
+for (b = 'A'; b <= 'Z'; b++)
 {
 putchar(b);
 }
