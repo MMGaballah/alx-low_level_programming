ceac7650b20efdbe2c4e9f1e397f9cfdc1573f63
Your Name <you@example.com>
2023-02-21 15:41:13 -0800

    6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  *
  * @n: is a variable;
  *
- * return:1, 0 and -1;
+ * Return:1, 0 and -1;
  */
 int print_sign(int n)
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - return vale is z
+ *int_abs(int) - get value of absolute integer
+ *@i: parameter to put in function
+ *Return: alawys z
+ */
+int _abs(int i)
+{
+int z;
+z = abs(i);
+return (z);
+}
