bb55ade6a68e98d1465c5f0ad171daeffbb7e982
Your Name <you@example.com>
2023-02-23 12:35:24 -0800

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *print_square - function to print square
+ *
+ *@size: a parameter
+ *
+ *Return: Always 0
+ */
+void print_square(int size)
+{
+int x = 0;
+int z;
+if (size == 0)
+{
+_putchar('\n');
+}
+else if (size < 0)
+{
+_putchar('\n');
+}
+else
+{
+while ( x < size)
+{
+z = 0;
+while (z < size)
+{ 
+_putchar('#');
+z++;
+}
+_putchar('\n');
+x++;
+}
+}
+}
