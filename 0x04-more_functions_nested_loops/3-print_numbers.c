d1cb92350f7d96706b679f10f80aa9aa3c7d255f
Your Name <you@example.com>
2023-02-23 10:36:04 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - function to print numbers
+ *
+ * _putchar - to print charcters one at a time;
+ * Return: Always 0;
+ */
+void print_numbers(void)
+{
+int x;
+for (x = 48; x <= 57; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
