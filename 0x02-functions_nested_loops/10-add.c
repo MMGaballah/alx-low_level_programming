47d20a22730a63c56090d5e63e4fcf6ede5f1ef7
Your Name <you@example.com>
2023-05-24 13:21:34 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers.
+ * @a: first integer added.
+ * @b: second integer added.
+ *
+ * Return: result of addition.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
