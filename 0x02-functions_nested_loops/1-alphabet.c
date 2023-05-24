0951cb44225b4cd021113f0cd4ace614fa411611
Your Name <you@example.com>
2023-05-24 07:34:47 -0700

    tasks 0 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet - a function to print chars
+ *
+ *Description: a printing function for alphabet using chars
+ *
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+int a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
