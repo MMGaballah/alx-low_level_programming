254b2c028387967a1b9ec40702af2aa04cf10bb4
Your Name <you@example.com>
2023-05-26 14:36:46 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers - a function to print digits
+ *
+ *Description: a printing function for digits 0 to 9
+ *
+ * Return: void.
+ */
+void more_numbers(void)
+{
+int a;
+int b;
+for ( b = 0; b < 10; b++)
+{
+for (a = 0; a <= 14; a++)
+{
+
+}
+_putchar('\n');
+}
+}	
+
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - print line - a function to print lines
+ *@n: given from main
+ *Description: a printing function for lines
+ *
+ * Return: void.
+ */
+void print_line(int n)
+{
+int s = 0;
+if (n > 0)
+{
+while (s <= n)
+{
+_putchar('_');
+s++;
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
