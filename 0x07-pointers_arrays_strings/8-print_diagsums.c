742c7c8095d8343f34979b0324ad493ed3ad0b80
Your Name <you@example.com>
2023-06-05 11:48:08 -0700

    tasl 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - print sum of two diagonals
+ *@a: a value
+ *@size: a second value
+ *
+ *
+ */
+ void print_diagsums(int *a, int size)
+{
+int c;
+int b;
+int sum1 = 0;
+int sum2 = 0;
+int s = size - 1;
+int v = 0;
+for (c = 0; c < size; c++)
+{
+for (b = 0; b < size; b++)
+{
+if (c == b)
+{
+sum1 = sum1 + a[v];
+}
+if (b == s)
+{
+sum2 = sum2 + a[v];
+}
+v++;
+}
+s--;
+}
+printf("%d,%d\n", sum1, sum2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif
